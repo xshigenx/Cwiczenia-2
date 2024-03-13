a2f8e6c89ddc707c35c9c9c37562fd646d28dc57
xshigenx <161622150+xshigenx@users.noreply.github.com>
2024-03-13 15:42:20 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+  public static void main(String[] args)
+  {
+    String myString = "Hello World";
+    for (int i = 0; i < myString.length(); i++)
+      {
+        System.out.printIn(myString.charAt(i));
+      }
+  }
+
+}
